4511e11e0d83634215753f04715d1a7fecc8f1ed
Mugilan Ganesan <49064156+MugilanGN@users.noreply.github.com>
2021-01-23 15:34:27 +0530

    Data for ML Model
    The processed data with the ingredients for the object recognition model.

---
+++ csvParser/parser.py.py
@@ -0,0 +1,44 @@
+import csv
+
+file = open("dump2.txt")
+
+ingredients_set = set()
+items_set = set()
+
+for i, line in enumerate(file):
+
+    ingredients = [item.strip() for item in line.split(';')[1].split(',')]
+    item = line.split(';')[0].strip().replace(' ',r'_').lower()
+
+    items_set.add(item)
+
+    for ingredient in ingredients:
+        ingredients_set.add(ingredient)
+
+file.close()
+
+print(ingredients_set)
+
+filename = "ingredients.csv"
+    
+with open(filename, 'w+', newline='') as csvfile:  
+    writer = csv.DictWriter(csvfile, fieldnames = ["item"] + list(items_set) + list(ingredients_set), restval=0)    
+    writer.writeheader()
+
+    for i,line in enumerate(open("dump2.txt")):
+        ingredients = [item.strip() for item in line.split(';')[1].split(',')]
+
+        ingredients_dict = {}
+
+        for ingredient in ingredients:
+            ingredients_dict[ingredient] = 1
+
+        item = line.split(';')[0].strip().replace(' ',r'_').lower()
+
+        row = {"item": item, item: 1,**ingredients_dict}
+
+        print(row)
+
+        writer.writerow(row)
+
+file.close()
