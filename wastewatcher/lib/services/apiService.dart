4f3fc75f117e7b7ca3a827aacc80f91cc41af0b2
DudeBro249 <yoayush@gmail.com>
2021-01-23 23:17:15 +0530

    added dish alertdialog and tweaked piechart ui

--- wastewatcher/lib/services/apiService.dart
+++ wastewatcher/lib/services/apiService.dart
@@ -1,11 +1,28 @@
 import 'package:flutter_riverpod/all.dart';
+import 'package:http/http.dart' as http;
 import 'package:wastewatcher/models/dish.dart';
+import 'package:wastewatcher/models/edamamApiResponse.dart';
+import 'package:meta/meta.dart';
+import 'dart:convert';
 
 final apiServiceProvider = Provider<APIService>((ref) {
   return APIService();
 });
 
 class APIService {
+  static const API_BASE_URL = 'https://312b16c6e903.ngrok.io';
+
+  Future<EdamamApiResponse> searchEdamamDishes({@required String query}) async {
+    final response = await http.get(
+      '$API_BASE_URL/dish-search/?query=$query',
+    );
+    return EdamamApiResponse.fromMap(
+      json.decode(
+        response.body,
+      ),
+    );
+  }
+
   Future<List<Dish>> getDishes() async {
     await Future.delayed(
       Duration(
@@ -17,6 +34,11 @@ class APIService {
         "name": "chocolate cake",
         "ingredients": ["cocoa", "bread"],
         "quantity": 5.0,
+      },
+      {
+        "name": "pizza",
+        "ingredients": ["cheese", "bread"],
+        "quantity": 3.0,
       },
     ];
     return dishesJson.map((dishJson) => Dish.fromMap(dishJson)).toList();
